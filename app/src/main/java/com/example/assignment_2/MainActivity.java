2ede2646726f0223d7c70557c8261b7d90dde929
Aliraza777 <Aliwahlah777@gmail.com>
2021-06-19 20:31:35 +0500

    toggle added navigation bar done

--- app/src/main/java/com/example/assignment_2/MainActivity.java
+++ app/src/main/java/com/example/assignment_2/MainActivity.java
@@ -20,25 +20,15 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Button btnsignuot;
-    private ActionBarDrawerToggle menubar;
-    private ToggleButton toggleButton;
+    private ActionBarDrawerToggle drawer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-//        androidx.appcompat.widget.Toolbar myToolbar = findViewById(R.id.appBar);
-//        setSupportActionBar(myToolbar);
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.mainDrawer);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this , drawer , myToolbar , R.string.app_name , R.string.app_name);
-//        drawer.setDrawerListener(toggle);
-//        toggle.syncState();
-//
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
-//        getActionBar().setHomeButtonEnabled(true);
-
-
-
+        setup_Views();
         initialize();
 
 
@@ -55,28 +45,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    void setup_Views(){
+        setUpDrawlayout();
+    }
+    public void setUpDrawlayout(){
+        setSupportActionBar(findViewById(R.id.appBar));
+        drawer= new ActionBarDrawerToggle(MainActivity.this,findViewById(R.id.mainDrawer),R.string.app_name,R.string.app_name);
+        drawer.syncState();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawer.onOptionsItemSelected(item)) {
+            return (true);
+        }
+        return super.onOptionsItemSelected(item);
+    }
     private void initialize() {
         btnsignuot = findViewById(R.id.signout);
     }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        if(menubar.onOptionsItemSelected(item)){
-//            return true;
-//        }
-//
-//    }
 
-//    @Override
-//    protected  void onPostCreate(Bundle savedInstanceState) {
-//
-//        super.onPostCreate(savedInstanceState);
-//        menubar.syncState();
-//    }
-//
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//        menubar.onConfigurationChanged(newConfig);
-//    }
 }
